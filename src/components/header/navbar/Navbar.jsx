423fffa3f22c00d091dcc6973276800acda2896c
ramtin.zand <ramtinzand6@gmail.com>
2025-02-08 15:32:39 +0900

    ramtin

--- src/components/header/navbar/Navbar.jsx
+++ src/components/header/navbar/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
         <button
           onClick={() => setOpenMenu(!OpenMenu)}
           type="button"
-          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          className="inline-flex items-center p-2 w-10 h-10 justify-center text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
           aria-expanded={OpenMenu}
         >
           <span className="sr-only">Open main menu</span>
