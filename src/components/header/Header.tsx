01629753b384cbe0fb8f77210758624f95779b79
ramtin.zand <ramtinzand6@gmail.com>
2025-02-08 15:54:10 +0900

    ramtin

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -10,11 +10,11 @@ const Header = () => {
        <img src={baner} className="w-full aspect-[21/9] object-cover" alt="baner.webp" />
      </div>
      {/* blue-part */}
-     <div className="bg-primary h-[30vh] w-full p-10 flex flex-col items-center justify-center text-white">
-       <h2 className="text-base md:text-6xl shadow">
+     <div className="bg-primary min-h-[80px] w-full p-10 flex flex-col items-center justify-center text-white">
+       <h2 className="text-base md:text-5xl shadow">
        خدمات نصب و اجرای داربست فلزی در کرمان
           </h2>
-       <p className="mt-10 ">بیش از دو دهه تجربه در اجرای داربست فلزی در کرمان</p>
+       <p className="mt-10 text-center">بیش از دو دهه تجربه در اجرای داربست فلزی در کرمان</p>
      </div>
    </header>
     </>
