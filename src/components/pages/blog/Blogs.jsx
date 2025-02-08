eb2b61b38c81e508c74c806dde25acaec022c609
ramtin.zand <ramtinzand6@gmail.com>
2025-02-08 16:52:14 +0900

    ramtin3

--- src/components/pages/blog/Blogs.jsx
+++ src/components/pages/blog/Blogs.jsx
@@ -1,9 +1,22 @@
 import { Link } from "react-router-dom";
 import blog from "./blogData";
+import { Helmet } from "react-helmet-async";
 
 const Blogs = () => {
   return (
     <div className="md:container md:mx-auto mx-2 my-10">
+       <Helmet>
+              <title>  مقالات | داربست فلزی در کرمان</title>
+              <meta
+                name="description"
+                content="شرکت خدمات داربست فلزی کرمان ارائه‌دهنده خدمات نصب، اجاره، و طراحی داربست فلزی با کیفیت بالا و مطابق استانداردهای ایمنی ایران. با ما بیشتر آشنا شوید."
+              />
+              <meta
+                name="keywords"
+                content=" داربست فلزی در کرمان ، درباره ما, شرکت خدمات داربست فلزی, نصب داربست کرمان, اجاره داربست, طراحی داربست فلزی"
+              />
+              <link rel="canonical" href="https://kermandarbast.ir/blogs" />
+            </Helmet>
       {blog.map((blog) => (
         <div
           key={blog.id}
